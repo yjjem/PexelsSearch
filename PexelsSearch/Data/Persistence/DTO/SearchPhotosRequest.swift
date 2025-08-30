2c83ff71eff06d9f29e9f6582a10856593ac66f3
yjjem <marksim0606@gmail.com>
2025-08-30 22:51:18 +0900

    refactor: SearchPhotosRequest is responsible for type conversion

--- PexelsSearch/Data/Persistence/DTO/SearchPhotosRequest.swift
+++ PexelsSearch/Data/Persistence/DTO/SearchPhotosRequest.swift
@@ -12,16 +12,16 @@ struct SearchPhotosRequest {
     let size: String
     let color: String
     let locale: String
-    let page: Int
-    let perPage: Int
+    let page: String
+    let perPage: String
     
     init(from searchQuery: SearchPhotosQuery, page: Int, perPage: Int) {
         self.query = searchQuery.query
         self.orientation = searchQuery.orientation.rawValue
         self.size = searchQuery.size.rawValue
         self.locale = searchQuery.locale
-        self.page = page
-        self.perPage = perPage
+        self.page = String(page)
+        self.perPage = String(perPage)
         
         switch searchQuery.color {
         case .supported(let photoSupportedColor):
