81ee8380d66efc12205ae069be38525cf4dec54d
yjjem <marksim0606@gmail.com>
2025-08-30 23:01:20 +0900

    feat: Add DefaultRemotePhotoPersistence

--- PexelsSearch/Data/Persistence/RemotePhotos/RemotePhotoPersistence.swift
+++ PexelsSearch/Data/Persistence/RemotePhotos/RemotePhotoPersistence.swift
@@ -7,9 +7,66 @@
 
 
 import Combine
+import Foundation
 
 protocol RemotePhotoPersistence {
     func fetchPhotos(
         _ request: SearchPhotosRequest
     ) -> AnyPublisher<SearchPhotosResponse, RemotePhotoError>
+}
+
+final class DefaultRemotePhotoPersistence: RemotePhotoPersistence {
+    
+    // MARK: Variable(s)
+    
+    private let httpClient: HTTPClient
+    private let decoder: JSONDecoder
+    
+    init(httpClient: HTTPClient) {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        self.decoder = decoder
+        self.httpClient = httpClient
+    }
+    
+    // MARK: Function(s)
+    
+    func fetchPhotos(
+        _ request: SearchPhotosRequest
+    ) -> AnyPublisher<SearchPhotosResponse, RemotePhotoError> {
+        let request = PexelsAPI.searchPhotosRequest(
+            query: request.query,
+            orientation: request.orientation,
+            size: request.size,
+            color: request.color,
+            locale: request.locale,
+            page: request.page,
+            perPage: request.perPage
+        )
+        return httpClient.loadPublisher(httpRequest: request)
+            .decode(type: SearchPhotosResponse.self, decoder: decoder)
+            .mapError { error in
+                switch error.self {
+                    case is DecodingError:
+                        return .decodingFailed
+                    case let httpClientError as HTTPClientError:
+                        switch httpClientError {
+                        case .badHTTPResponse(let httpResponse):
+                            return .serverError(statusCode: httpResponse.statusCode)
+                        case .notHttpResponse(_), .requestConversionFailure(_), .unexpected(_):
+                            return .unexpected()
+                        case .urlError(let urlError):
+                            switch urlError.code {
+                            case .networkConnectionLost, .notConnectedToInternet:
+                                return .networkUnavailable
+                        default:
+                                return .unexpected()
+                        }
+                    }
+                    default:
+                    return .unexpected()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
 }
