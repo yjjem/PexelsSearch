2e6cae5a88fe49d225fa1ff4b48d76e910428f4f
yjjem <marksim0606@gmail.com>
2025-09-13 18:05:43 +0900

    feat: Add PhotoCellContentView

---
+++ PexelsSearch/Presentation/SearchPhotos/PhotoCellContentView.swift
@@ -0,0 +1,95 @@
+//
+//  PhotoCellContentView.swift
+//  PexelsSearch
+//
+//  Created by Remy Park on 9/13/25.
+//
+
+
+import UIKit
+import Combine
+
+// MARK: UICollectionViewCell
+
+extension UICollectionViewCell {
+    func photoCellConfiguration() -> PhotoCellContentView.Configuration {
+        return PhotoCellContentView.Configuration()
+    }
+}
+
+final class PhotoCellContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var name: String = ""
+        var imagePublisher: AnyPublisher<UIImage, Never>?
+        
+        func makeContentView() -> any UIView & UIContentView {
+            return PhotoCellContentView(configuration: self)
+        }
+        
+        func updated(for state: any UIConfigurationState) -> Self {
+            return self
+        }
+    }
+    
+    // MARK: Property(s)
+    
+    var configuration: any UIContentConfiguration {
+        didSet {
+            configure(with: configuration)
+        }
+    }
+    
+    private let imageView = LoadableImageView()
+    private let nameLabel = PaddableLabel()
+    
+    init(configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        super.init(frame: .zero)
+        configureLayout()
+        configure(with: configuration)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Private Function(s)
+    
+    private func configureLayout() {
+        addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 4
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.heightAnchor
+                .constraint(equalTo: imageView.widthAnchor, multiplier: 1.0),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func configure(with configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else {
+            return
+        }
+        self.imageView.layer.borderColor = UIColor.systemGray.cgColor
+        self.imageView.layer.borderWidth = 0.2
+        self.imageView.layer.masksToBounds = true
+        DispatchQueue.global().async {
+            if let url = URL(string: "https://picsum.photos/250/250"),
+               let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: data)
+                    self.nameLabel.text = configuration.name
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PhotoCellContentView(configuration: PhotoCellContentView.Configuration())
+}
