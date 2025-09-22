47f3f74ed3d6c13f9130b042bb831c878cfb3a2d
yjjem <marksim0606@gmail.com>
2025-09-23 00:21:09 +0900

    feat: Add DefaultFetchPhotoUseCase implementation

--- PexelsSearch/Domain/UseCase/FetchPhoto/FetchPhotoUseCase.swift
+++ PexelsSearch/Domain/UseCase/FetchPhoto/FetchPhotoUseCase.swift
@@ -11,3 +11,21 @@ import Combine
 protocol FetchPhotoUseCase {
     func execute(for photoIdentifier: Int) -> AnyPublisher<Photo, FetchPhotoError>
 }
+
+final class DefaultFetchPhotoUseCase: FetchPhotoUseCase {
+    
+    // MARK: Property(s)
+    
+    private let repository: PhotoRepository
+    
+    init(repository: PhotoRepository) {
+        self.repository = repository
+    }
+    
+    // MARK: Function(s)
+    
+    func execute(for photoIdentifier: Int) -> AnyPublisher<Photo, FetchPhotoError> {
+        return repository.fetchPhoto(by: photoIdentifier)
+            .eraseToAnyPublisher()
+    }
+}
