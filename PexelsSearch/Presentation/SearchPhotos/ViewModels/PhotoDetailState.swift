ad9476ec6ccd2617d2b04e2221650cccb9f55456
yjjem <marksim0606@gmail.com>
2025-09-24 01:40:39 +0900

    feat: Add PhotoDetailState

---
+++ PexelsSearch/Presentation/SearchPhotos/ViewModels/PhotoDetailState.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoDetailState.swift
+//  PexelsSearch
+//
+//  Created by Remy Park on 9/24/25.
+//
+
+
+struct PhotoDetailState {
+    let photoURL: String
+    let photoIdentifier: Int
+    let providerName: String
+    let description: String
+    let sizeDisplayText: String
+}
