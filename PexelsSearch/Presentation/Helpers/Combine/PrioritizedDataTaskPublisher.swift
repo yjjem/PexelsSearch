f478081945480ffa2c8bcb75989c0e8867b82ee3
yjjem <marksim0606@gmail.com>
2025-10-15 23:16:41 +0900

    feat: Add PrioritizedDataTaskPublisher

---
+++ PexelsSearch/Presentation/Helpers/Combine/PrioritizedDataTaskPublisher.swift
@@ -0,0 +1,70 @@
+//
+//  PrioritizedDataTaskPublisher.swift
+//  PexelsSearch
+//
+//  Created by Remy Park on 10/15/25.
+//
+
+
+import Combine
+import Foundation
+
+struct PrioritizedDataTaskPublisher : Publisher, Sendable {
+    typealias Output = (data: Data, response: URLResponse)
+    typealias Failure = Error
+    
+    let request: URLRequest
+    let session: URLSession
+    
+    init(request: URLRequest, session: URLSession) {
+        self.request = request
+        self.session = session
+    }
+    
+    func receive<S>(
+        subscriber: S
+    ) where S : Subscriber, S.Failure == Error, S.Input == (data: Data, response: URLResponse) {
+        let subscription = URLSessionSubscription(
+            subscriber: subscriber,
+            urlSession: session,
+            urlRequest: request
+        )
+        return subscriber.receive(subscription: subscription)
+    }
+}
+
+final class URLSessionSubscription<SubscriberType: Subscriber>: Subscription where SubscriberType.Input == (data: Data, response: URLResponse), SubscriberType.Failure == Error {
+    private var subscriber: SubscriberType?
+    private let urlSession: URLSession
+    private let urlRequest: URLRequest
+    private let taskPriority: Float
+    
+    init(
+        subscriber: SubscriberType?,
+        urlSession: URLSession,
+        urlRequest: URLRequest,
+        taskPriority: Float = URLSessionDataTask.defaultPriority
+    ) {
+        self.subscriber = subscriber
+        self.urlSession = urlSession
+        self.urlRequest = urlRequest
+        self.taskPriority = taskPriority
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        let task = urlSession.dataTask(with: urlRequest) { [weak self] (data, response, error) in
+            if let error {
+                self?.subscriber?.receive(completion: .failure(error))
+            }
+            if let data, let response {
+                _ = self?.subscriber?.receive((data, response))
+            }
+        }
+        task.priority = taskPriority
+        task.resume()
+    }
+    
+    func cancel() {
+        subscriber = nil
+    }
+}
