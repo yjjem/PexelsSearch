93df3f4ee6a23b72130f5d9c13a62fe91ef0766a
yjjem <marksim0606@gmail.com>
2025-10-22 20:54:29 +0900

    feat: Add LikedPhoto Entity

---
+++ PexelsSearch/Domain/Entity/LikedPhoto.swift
@@ -0,0 +1,18 @@
+//
+//  LikedPhoto.swift
+//  PexelsSearch
+//
+//  Created by Remy Park on 10/22/25.
+//
+
+
+import Foundation
+
+struct LikedPhoto {
+    let id: Int
+    let url: String
+    let photoSize: String
+    let description: String
+    let photographerName: String
+    let likedAt: Date
+}
