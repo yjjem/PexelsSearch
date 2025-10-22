2281c11341600cc72c4e7ef418fc58e9b29bf3b2
yjjem <marksim0606@gmail.com>
2025-10-22 21:22:13 +0900

    feat: Add LikePhotoUseCase default implementation

--- PexelsSearch/Domain/UseCase/LikePhotoUseCase/LikePhotoUseCase.swift
+++ PexelsSearch/Domain/UseCase/LikePhotoUseCase/LikePhotoUseCase.swift
@@ -10,3 +10,32 @@ import Combine
 protocol LikePhotoUseCase {
     func execute(_ photoID: Int)
 }
+
+final class DefaultLikePhotoUseCase: LikePhotoUseCase {
+    
+    // MARK: Property(s)
+    
+    private var cancelBag: Set<AnyCancellable> = .init()
+    private let repository: LikedPhotosRepository
+    private let photoRepository: PhotoRepository
+    
+    init(repository: LikedPhotosRepository, photoRepository: PhotoRepository) {
+        self.repository = repository
+        self.photoRepository = photoRepository
+    }
+    
+    // MARK: Function(s)
+    
+    func execute(_ photoID: Int) {
+        photoRepository
+            .fetchPhoto(by: photoID)
+            .map { LikedPhoto(photo: $0) }
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { [weak self] newLikedPhoto in
+                    self?.repository.create(newLikedPhoto)
+                }
+            )
+            .store(in: &cancelBag)
+    }
+}
