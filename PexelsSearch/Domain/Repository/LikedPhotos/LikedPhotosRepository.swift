08e75e9150daae0804aa811c76f5ddf95336f773
yjjem <marksim0606@gmail.com>
2025-10-24 02:05:39 +0900

    feat: implement default LikedPhotosRepository

--- PexelsSearch/Domain/Repository/LikedPhotos/LikedPhotosRepository.swift
+++ PexelsSearch/Domain/Repository/LikedPhotos/LikedPhotosRepository.swift
@@ -19,3 +19,44 @@ protocol LikedPhotosRepository {
     func readAll() -> AnyPublisher<[LikedPhoto], LikedPhotoRepositoryError>
     func delete(_ id: Int)
 }
+
+final class DefaultLikedPhotoRepository: LikedPhotosRepository {
+    
+    // MARK: Property(s)
+    
+    private let likedPhotoStorage: LikedPhotoStorage
+    
+    init(likedPhotoStorage: LikedPhotoStorage) {
+        self.likedPhotoStorage = likedPhotoStorage
+    }
+    
+    // MARK: Function(s)
+    
+    func create(_ newLikedPhoto: LikedPhoto) {
+        try? likedPhotoStorage.create(newLikedPhoto)
+    }
+    
+    func read(_ id: Int) -> AnyPublisher<LikedPhoto, LikedPhotoRepositoryError> {
+        guard let likedPhoto = try? likedPhotoStorage.read(id: id)?.toDomain()  else {
+            return Fail(error: LikedPhotoRepositoryError.notFound)
+                .eraseToAnyPublisher()
+        }
+        return Just(likedPhoto)
+            .setFailureType(to: LikedPhotoRepositoryError.self)
+            .eraseToAnyPublisher()
+    }
+    
+    func readAll() -> AnyPublisher<[LikedPhoto], LikedPhotoRepositoryError> {
+        guard let allLikedPhotos = try? likedPhotoStorage.readAll() else {
+            return Fail(error: LikedPhotoRepositoryError.notFound)
+                .eraseToAnyPublisher()
+        }
+        return Just(allLikedPhotos.map { $0.toDomain() })
+            .setFailureType(to: LikedPhotoRepositoryError.self)
+            .eraseToAnyPublisher()
+    }
+    
+    func delete(_ id: Int) {
+        try? likedPhotoStorage.delete(id: id)
+    }
+}
